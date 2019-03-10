052ad09f1f2827c42f612831607f9050c0fcc9c4
Priyantha Gunawardena <pgdesigning@gmail.com>
2019-03-10 00:22:42 -0600

    refactoring the build process

---
+++ src/__tests__/nodecft.test.js
@@ -0,0 +1,8 @@
+
+import { init } from '../nodecft'
+describe('Test NodeCFT', () => {
+    test('createStack should shoul execute without any issue', async () => {
+        init();
+        console.log(process.env)
+    })
+  })
